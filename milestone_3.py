b1e782ebdcf457e6ca92a55cfcc4e62ee8aef4fd
Martin Klefas-Stennett <cleverbum@hotmail.com>
2023-02-28 19:25:27 +0000

    Task 2 - attempt 1

--- milestone_3.py
+++ milestone_3.py
@@ -1,3 +1,10 @@
+import random
+import re
+
+word_list = ["Apple","Banana","Pear","Orange","Plum"]
+
+word = random.choice(word_list)
+
 while True:
     guess = input("Please guess a single letter")
 
@@ -8,3 +15,8 @@ while True:
             print("Invalid letter. Please, enter a single alphabetical character.")
     except:
         print("Invalid letter. Please, enter a single alphabetical character.")
+
+if guess in word:
+    print("Good guess! %s is in the word.", guess)
+else:
+    print("Sorry, %s is not in the wordT Try again.", guess)
