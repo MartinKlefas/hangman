749a7e873767032af028f4b1839a07dd9912cfcd
Martin Klefas-Stennett <cleverbum@hotmail.com>
2023-02-28 21:12:48 +0000

    partway through task 1

--- milestone_4.py
+++ milestone_4.py
@@ -14,4 +14,28 @@ class Hangman:
             underscore_list.append("_")
 
          self.word_guesses = underscore_list
-         
+
+    def check_guess(thisGuess):
+    thisGuess = thisGuess.lower()
+    if thisGuess in word.lower():
+        print("Good guess! %s is in the word." % thisGuess)
+    else:
+        print("Sorry, %s is not in the wordT Try again." % thisGuess)
+
+    def ask_for_input():
+        while True:
+            guess = input("Please guess a single letter: ")
+
+            try:
+                if not len(guess) == 1 or not guess.isalpha():  
+                    print("Invalid letter. Please, enter a single alphabetical character.")
+                elif guess in self.list_of_guesses:
+                    print("You aleady tried that letter!")
+                else:
+                    check_guess(guess)
+                
+                self.list_of_guesses.add
+                
+            except:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(guess)
