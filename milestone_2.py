e9a02c5124eb66560f601581463804a4bb90e004
Martin Klefas-Stennett <cleverbum@hotmail.com>
2023-02-28 18:55:01 +0000

    Task 4

--- milestone_2.py
+++ milestone_2.py
@@ -5,3 +5,11 @@ word = random.choice(word_list)
 print(word)
 
 guess = input("Please guess a single letter")
+
+try:
+    if len(guess) == 1 :
+        print("Good Guess")
+    else:
+        print("Oops! that is not a valid input")
+except:
+    print("Oops! that is not a valid input")
