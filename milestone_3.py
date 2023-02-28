e073522925922826a82d9c34cdf8c320a382e351
Martin Klefas-Stennett <cleverbum@hotmail.com>
2023-02-28 20:00:57 +0000

    Task 3 attempt 1

--- milestone_3.py
+++ milestone_3.py
@@ -1,22 +1,35 @@
 import random
 import re
 
+def check_guess(thisGuess):
+    thisGuess = thisGuess.lower()
+    if thisGuess in word.lower:
+        print("Good guess! %s is in the word.", thisGuess)
+    else:
+        print("Sorry, %s is not in the wordT Try again.", thisGuess)
+
+def ask_for_input():
+    while True:
+        guess = input("Please guess a single letter")
+
+        try:
+            if len(guess) == 1 and guess.isalpha():  #re.search([a-zA-Z],guess) :
+                break
+            else:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+        except:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(guess)
+
+
+
+
+
+
 word_list = ["Apple","Banana","Pear","Orange","Plum"]
 
 word = random.choice(word_list)
 
-while True:
-    guess = input("Please guess a single letter")
+ask_for_input()
 
-    try:
-        if len(guess) == 1 and guess.isalpha():  #re.search([a-zA-Z],guess) :
-            break
-        else:
-            print("Invalid letter. Please, enter a single alphabetical character.")
-    except:
-        print("Invalid letter. Please, enter a single alphabetical character.")
 
-if guess in word:
-    print("Good guess! %s is in the word.", guess)
-else:
-    print("Sorry, %s is not in the wordT Try again.", guess)
