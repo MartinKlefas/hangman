a83e745b2048d8f1c4766ac158eb466ee78c0919
Martin Klefas-Stennett <cleverbum@hotmail.com>
2023-02-28 20:42:42 +0000

    Milestone 4 task 1 maybe

--- milestone_4.py
+++ milestone_4.py
@@ -1,4 +1,17 @@
+import random
+
 class Hangman:
     def __init__(self, word_list, num_lives = 5) -> None:
          self.word_list = word_list
          self.num_lives = num_lives
+         self.word = random.choice(word_list)
+         self.num_letters = len(self.word)
+         self.list_of_guesses = list()
+
+         underscore_list = list()
+
+         for letter in self.word:
+            underscore_list.append("_")
+
+         self.word_guesses = underscore_list
+         
